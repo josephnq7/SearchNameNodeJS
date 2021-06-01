7e1915318604cc1d69076b1cd7fb886c15717578
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2021-05-31 18:42:12 -0700

    defining routes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //1. create a web server
 var http = require('http');
+var router = require('./router.js')
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n Joseph \n');
+    router.home(request, response);
+    router.user(request, response);
 }).listen(3450, '127.0.0.1');
+
+
 
 console.log('Server running at http://127.0.0.1:3450');
