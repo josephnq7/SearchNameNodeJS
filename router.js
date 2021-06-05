96a21387c3900f7469ff1a982e33757c5a09abf0
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2021-06-04 23:23:47 -0700

    finished

--- router.js
+++ router.js
@@ -1,20 +1,35 @@
 var Profile = require("./profile.js");
 var render = require('./renderer.js');
+var querystring = require('querystring');
+
+var commonHeader = {'Content-Type': 'text/html'};
 
 function home(request, response) {
     if (request.url === '/') {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        render.view('header', {}, response);
-        render.view('search', {}, response);
-        render.view('footer', {}, response);
-        response.end();
+        if (request.method.toLowerCase() == 'get') {
+            response.writeHead(200, commonHeader);
+            render.view('header', {}, response);
+            render.view('search', {}, response);
+            render.view('footer', {}, response);
+            response.end();
+        } else {
+            //POST
+            //get the post data from body, extract the username and redirect to /username
+            request.on('data', function(postBody) {
+                var query = querystring.parse(postBody.toString());
+                response.writeHead(303, {"Location": "/" + query.username});
+                response.end();
+            });
+
+        }
+
     }
 }
 
 function user(request, response) {
     var username = request.url.replace('/', '');
     if (username.length > 0) {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.writeHead(200, commonHeader);
         render.view('header', {}, response);
 
         //get json from Treehouse
